1c713a8dfa3b5fef5fddf2900f842f85cd66c906
Jaime Takikawa and Jason Huels <jaime+jason@>
2019-08-20 15:16:35 -0700

    make the prime sifter work

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,47 @@
 $(function() {
   $(".form").submit(function(event) {
     event.preventDefault();
-    var number = $("#input").val();
+    var number = parseInt($("#input").val());
     var numbers = [];
     var prime = 2;
-    var primes = [prime];
+    var primes = [];//[prime];
 
 
     for(let i=prime; i<=number; i++) {
       numbers.push(i);
     }
 
-    for(let i=prime; i<=numbers.length; i++) {
-      for(let j=i; j<=numbers.length; j+=i) {
-        numbers.splice(j, 1);
-        console.log(numbers);
+    // for(let i=2; i<numbers.length; i++) {
+    //   numbers.splice(i, 1);
+    // }
+
+    // for(let i=2; i<numbers.length; i++){
+    //   if(numbers[i] % 2 === 0) {
+    //     numbers.splice(i, 1);
+    //   }
+    // }
+    // for(let i=3; i<numbers.length; i++){
+    //   if(numbers[i] % 3 === 0) {
+    //     numbers.splice(i, 1);
+    //   }
+    // }
+    // for(let i=5; i<numbers.length; i++){
+    //   if(numbers[i] % 5 === 0) {
+    //     numbers.splice(i, 1);
+    //   }
+    // }
+    // for(let i=7; i<numbers.length; i++){
+    //   if(numbers[i] % 7 === 0) {
+    //     numbers.splice(i, 1);
+    //   }
+    // }
+
+    for(let j=0; j<numbers.length; j++) {
+      primes.push(numbers[j]);
+      for(let i=primes[j]; i<numbers.length; i++) {
+        if(numbers[i] % primes[j] === 0) {
+          numbers.splice(i, 1);
+        }
       }
     }
 
