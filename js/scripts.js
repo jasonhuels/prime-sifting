279c3450d1cb9b1f0dc26c2565045cecbcad814c
Jason Huels and Jaime Takikawa <jason@>
2019-08-20 15:52:46 -0700

    remove commented out code

--- js/scripts.js
+++ js/scripts.js
@@ -4,37 +4,11 @@ $(function() {
     var number = parseInt($("#input").val());
     var numbers = [];
     var prime = 2;
-    var primes = [];//[prime];
-
+    var primes = [];
 
     for(let i=prime; i<=number; i++) {
       numbers.push(i);
     }
-
-    // for(let i=2; i<numbers.length; i++) {
-    //   numbers.splice(i, 1);
-    // }
-
-    // for(let i=2; i<numbers.length; i++){
-    //   if(numbers[i] % 2 === 0) {
-    //     numbers.splice(i, 1);
-    //   }
-    // }
-    // for(let i=3; i<numbers.length; i++){
-    //   if(numbers[i] % 3 === 0) {
-    //     numbers.splice(i, 1);
-    //   }
-    // }
-    // for(let i=5; i<numbers.length; i++){
-    //   if(numbers[i] % 5 === 0) {
-    //     numbers.splice(i, 1);
-    //   }
-    // }
-    // for(let i=7; i<numbers.length; i++){
-    //   if(numbers[i] % 7 === 0) {
-    //     numbers.splice(i, 1);
-    //   }
-    // }
 
     for(let j=0; j<numbers.length; j++) {
       primes.push(numbers[j]);
